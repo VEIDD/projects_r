cda93df4b2f5d6e98c62a53fb8d6e6b28f5b94e6
VEIDD <separik3@gmail.com>
2025-06-21 14:43:28 +0300

    fix

---  Temperature Converter /app.js
+++  Temperature Converter /app.js
@@ -1,36 +1,15 @@
-let dropdown1 = document.querySelector(".dropdown");
-let list1 = document.querySelector(".list1");
-let answers1 = list1.querySelectorAll("li");
-let title1 = dropdown1.querySelector("p");
+let dropdowns = document.querySelectorAll(".dropdown");
 
-let dropdown2 = document.querySelector(".dropdown2");
-let list2 = document.querySelector(".list2");
-let answers2 = list2.querySelectorAll("li");
-let title2 = dropdown2.querySelector("p");
-
-let check1 = 0;
-let check2 = 0;
-
-dropdown1.addEventListener("click", () => {
-  list1.classList.toggle("hidden");
-});
-answers1.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    check1 = 1;
-    check(check1, check2);
-    title1.textContent = btn.textContent;
-  });
-});
-
-dropdown2.addEventListener("click", () => {
-  list2.classList.toggle("hidden");
-});
-
-answers2.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    check2 = 1;
-    check(check1, check2);
-    title2.textContent = btn.textContent;
+dropdowns.forEach((el) => {
+  el.addEventListener("click", () => {
+    const list = el.querySelector(".list");
+    list.classList.toggle("hidden");
+    list.querySelectorAll("li").forEach((element) => {
+      element.addEventListener("click", () => {
+        el.querySelector("p").textContent = element.textContent;
+        check();
+      });
+    });
   });
 });
 
@@ -39,53 +18,53 @@ let input = document.querySelector(".input");
 let content = document.querySelector(".content");
 let result = 0;
 
-function check(b, c) {
-  if (input.value !== "0.00" && b !== 0 && c !== 0) {
+function check() {
+  let title1 = document.querySelector(".check1");
+  let title2 = document.querySelector(".check2");
+  if (
+    input.value !== "0.00" &&
+    title1.textContent !== "From Unit" &&
+    title2.textContent !== "To Unit"
+  ) {
     btn_convert.style = `pointer-events: all;`;
 
-    btn_convert.addEventListener("click", () => {
-      if (document.querySelector(".result")) {
-        document.querySelector(".result").remove();
-      }
-      if (
-        title1.textContent === "Celseus" &&
-        title2.textContent === "Fahrenhreit"
-      ) {
-        result = (+input.value * 9) / 5 + 32;
-      }
-      if (
-        title1.textContent === "Fahrenhreit" &&
-        title2.textContent === "Celseus"
-      ) {
-        result = ((+input.value - 32) * 5) / 9;
-      }
-      if (title1.textContent === "Celseus" && title2.textContent === "Kelvin") {
-        result = +input.value + 273.15;
-      }
-      if (title1.textContent === "Kelvin" && title2.textContent === "Celseus") {
-        result = +input.value - 273.15;
-      }
-      if (
-        title1.textContent === "Kelvin" &&
-        title2.textContent === "Fahrenhreit"
-      ) {
-        result = ((+input.value - 273.15) * 9) / 5 + 32;
-      }
-      if (
-        title1.textContent === "Fahrenhreit" &&
-        title2.textContent === "Kelvin"
-      ) {
-        result = ((+input.value - 32) * 5) / 9 + 273.15;
-      }
-      if (title1.textContent === title2.textContent) {
-        result = +input.value;
-      }
+    if (
+      title1.textContent === "Celseus" &&
+      title2.textContent === "Fahrenhreit"
+    ) {
+      result = (+input.value * 9) / 5 + 32;
+    }
+    if (
+      title1.textContent === "Fahrenhreit" &&
+      title2.textContent === "Celseus"
+    ) {
+      result = ((+input.value - 32) * 5) / 9;
+    }
+    if (title1.textContent === "Celseus" && title2.textContent === "Kelvin") {
+      result = +input.value + 273.15;
+    }
+    if (title1.textContent === "Kelvin" && title2.textContent === "Celseus") {
+      result = +input.value - 273.15;
+    }
+    if (
+      title1.textContent === "Kelvin" &&
+      title2.textContent === "Fahrenhreit"
+    ) {
+      result = ((+input.value - 273.15) * 9) / 5 + 32;
+    }
+    if (
+      title1.textContent === "Fahrenhreit" &&
+      title2.textContent === "Kelvin"
+    ) {
+      result = ((+input.value - 32) * 5) / 9 + 273.15;
+    }
+    if (title1.textContent === title2.textContent) {
+      result = +input.value;
+    }
 
-      let p = document.createElement("p");
-      p.classList.add("result");
-      p.textContent = `${input.value} ${title1.textContent} is ${result.toFixed(
-        2
-      )} ${title2.textContent}`;
+    btn_convert.addEventListener("click", () => {
+      let p = document.querySelector(".result");
+      p.textContent = `${input.value} ${title1.textContent} is ${result.toFixed(2)} ${title2.textContent}`;
       content.append(p);
     });
   }
